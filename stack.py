dabe0af1285a90888dd2a9c6465a4bb241fdea3d
Deebakkarthi C R <dbk@deebakkarthi.com>
2022-07-13 00:25:53 +0530

    Deletions

--- stack.py
+++ stack.py
@@ -1,6 +1,26 @@
-from sll import sll
+from sll import sll, list_node
 
 class stack:
     def __init__(self):
         self.list = sll()
-        self.size = 0
+
+    def push(self, x):
+        self.list.insert_head(list_node(x))
+
+    def pop(self):
+        if self:
+            return self.list.remove_head().val
+        else:
+            return None
+
+    def top(self):
+        if self:
+            return self.list.head.val
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.list)
+
+    def __bool__(self):
+        return bool(self.list)
